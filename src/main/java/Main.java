5599861d973940860f5e3e1613ca884ee0327e68
aditya.nehra <aditya.nehra@thermofisher.com>
2018-06-02 10:08:17 +0530

    adding global and local configuration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -70,36 +70,128 @@ public class Main {
         System.out.println(personNameParts.getFirstName());
 
         /*
-        * Map Nested field
-        *
-        * Following on from the preceding collections examples, assume that inside our source data object, there is
-        * another Data Transfer Object (DTO) that holds the values we want to map.
-        * */
+         * Map Nested field
+         *
+         * Following on from the preceding collections examples, assume that inside our source data object, there is
+         * another Data Transfer Object (DTO) that holds the values we want to map.
+         * */
         MapperFactory mapperFactory3 = new DefaultMapperFactory.Builder().build();
         mapperFactory3.classMap(PersonContainer.class, PersonNameParts.class)
-                .field("name.firstName","firstName")
-                .field("name.lastName","lastName")
+                .field("name.firstName", "firstName")
+                .field("name.lastName", "lastName")
                 .register();
         MapperFacade mapperFacade3 = mapperFactory3.getMapperFacade();
-        PersonContainer personContainer = new PersonContainer(new Name("Nick","Canon"));
-        PersonNameParts personNameParts1 = mapperFacade3.map(personContainer,PersonNameParts.class);
+        PersonContainer personContainer = new PersonContainer(new Name("Nick", "Canon"));
+        PersonNameParts personNameParts1 = mapperFacade3.map(personContainer, PersonNameParts.class);
         System.out.println(personNameParts1.getLastName());
 
         /*
-        * Mapping NULL values
-        *
-        * In some cases, you may wish to control whether nulls are mapped or ignored when they are encountered.
-        * By default, Orika will map null values when encountered:
-        * */
+         * Mapping NULL values
+         *
+         * In some cases, you may wish to control whether nulls are mapped or ignored when they are encountered.
+         * By default, Orika will map null values when encountered:
+         * */
         MapperFactory mapperFactory4 = new DefaultMapperFactory.Builder().build();
-        mapperFactory4.classMap(NameSource.class,NameDest.class)
-                .field("firstName","name")
-                .field("lastName","surName")
+        mapperFactory4.classMap(NameSource.class, NameDest.class)
+                .field("firstName", "name")
+                .field("lastName", "surName")
                 .byDefault()
                 .register();
         MapperFacade mapperFacade4 = mapperFactory4.getMapperFacade();
-        NameSource nameSource = new NameSource("aditya",null);
-        NameDest nameDest = mapperFacade4.map(nameSource,NameDest.class);
+        NameSource nameSource = new NameSource("aditya", null);
+        NameDest nameDest = mapperFacade4.map(nameSource, NameDest.class);
         System.out.println(nameDest.getName() + " :: " + nameDest.getSurName());
+
+
+        /*
+         * Global Configuration
+         *
+         * We can configure our mapper to map nulls or ignore them at the global level before creating the global
+         * MapperFactory.
+         * */
+        MapperFactory mapperFactory5 = new DefaultMapperFactory.Builder().mapNulls(false).build();
+        mapperFactory5.classMap(NameSource.class, NameDest.class)
+                .field("firstName", "name")
+                .field("lastName", "surName")
+                .byDefault()
+                .register();
+        MapperFacade mapperFacade5 = mapperFactory5.getMapperFacade();
+        NameSource nameSource1 = new NameSource("mike", null);
+        NameDest nameDest1 = new NameDest("hilliary", "clinton");
+        mapperFacade5.map(nameSource1, nameDest1);
+        //What happens is that, by default, nulls are mapped. This means that even if a field value in the source object
+        //is null and the corresponding field’s value in the destination object has a meaningful value, it will be overwritten.
+        System.out.println(nameDest1.getName() + " :: " + nameDest1.getSurName());
+
+
+        /*
+         * Local Configuration
+         *
+         * Mapping of null values can be controlled on a ClassMapBuilder by using the mapNulls(true|false) or
+         * mapNullsInReverse(true|false) for controlling mapping of nulls in the reverse direction.
+         *
+         * By setting this value on a ClassMapBuilder instance, all field mappings created on the same ClassMapBuilder,
+         * after the value is set, will take on that same value.
+         * */
+        MapperFactory mapperFactory6 = new DefaultMapperFactory.Builder().build();
+        mapperFactory6.classMap(NameSource.class, NameDest.class)
+                .field("firstName", "name")
+                .mapNulls(false)
+                .field("lastName", "surName")
+                .byDefault()
+                .register();
+        MapperFacade mapperFacade6 = mapperFactory6.getMapperFacade();
+        NameSource nameSource2 = new NameSource("aditya", null);
+        NameDest nameDest2 = new NameDest("hilliary", "clinton");
+        mapperFacade6.map(nameSource2, nameDest2);
+        // Notice how we call mapNulls just before registering lastName field, this will cause all fields following the
+        // mapNulls call to be ignored when they have null value.
+        System.out.println(nameDest2.getName() + " :: " + nameDest2.getSurName());
+
+        NameSource nameSource3 = new NameSource(null, "nehra");
+        NameDest nameDest3 = new NameDest("hilliary", "clinton");
+        mapperFacade6.map(nameSource3, nameDest3);
+        // Notice how we call mapNulls after registering firstName field, this will cause null of firstName field to
+        //copy to name field in nameDest3.
+        System.out.println(nameDest3.getName() + " :: " + nameDest3.getSurName());
+
+        /*
+         * Bi-Directional mapping also accepts mapped null values
+         * */
+        MapperFactory mapperFactory7 = new DefaultMapperFactory.Builder().build();
+        mapperFactory7.classMap(NameSource.class, NameDest.class)
+                .field("firstName", "name")
+                .field("lastName", "surName")
+                .register();
+        MapperFacade mapperFacade7 = mapperFactory7.getMapperFacade();
+        NameSource nameSource4 = new NameSource("aditya", "nehra");
+        NameDest nameDest4 = new NameDest(null, "cilton");
+        mapperFacade7.map(nameDest4, nameSource4);
+        System.out.println(nameSource4.getFirstName() + " :: " + nameSource4.getLastName());
+
+
+        /*
+         * We can prevent this by calling mapNullInReverse and passing in false
+         * */
+        MapperFactory mapperFactory8 = new DefaultMapperFactory.Builder().build();
+        mapperFactory8.classMap(NameSource.class, NameDest.class)
+                .field("firstName", "name")
+                .mapNullsInReverse(false)
+                .field("lastName", "surName")
+                .register();
+        MapperFacade mapperFacade8 = mapperFactory8.getMapperFacade();
+        NameSource nameSource5 = new NameSource("aditya", "nehra");
+        NameDest nameDest5 = new NameDest(null, "cilton");
+        mapperFacade8.map(nameDest5, nameSource5);
+        //Here null will be mapped because mapNullsInReverse is done false after mapping 'firstName' to 'name'.
+        System.out.println(nameSource5.getFirstName() + " :: " + nameSource5.getLastName());
+
+        NameSource nameSource6 = new NameSource("aditya", "nehra");
+        NameDest nameDest6 = new NameDest("hilliary", null);
+        mapperFacade8.map(nameDest6, nameSource6);
+        //Here null will not be mapped because mapNullsInReverse is done false before mapping 'lastname' to 'surName'
+        System.out.println(nameSource6.getFirstName() + " :: " + nameSource6.getLastName());
+
+
     }
 }
